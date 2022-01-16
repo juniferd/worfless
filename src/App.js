2c81c2db6f981473cadcf70382b4ab06293c2346
jun <jun@home>
2022-01-15 17:29:17 -0800

    add nonsense proptypes so can at least build app add resolution for mini-css-extract-plugin - see https://github.com/facebook/create-react-app/issues/11930

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Tile, {TileWrapper} from './Tile'
 import './App.css'
 import bag from './letters'
--- src/Tile.js
+++ src/Tile.js
@@ -9,12 +9,22 @@ export function TileWrapper({children}) {
   )  
 }
 
+TileWrapper.propTypes = {
+  children: React.propTypes.any,
+}
+
 function Tile({ id, letter, found }) {
   return (
     <span className={[found ? styles.found : '', styles.tile].join(' ')} id={id}>
       {letter}
     </span>
   )
+}
+
+Tile.propTypes = {
+  id: React.propTypes.string,
+  letter: React.propTypes.string,
+  found: React.propTypes.bool,
 }
 
 export default Tile
